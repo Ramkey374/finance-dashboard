7aeffce204ad528e1c379a7c55b3dc07c8bf3687
Ramkey374 <gudaramkrishna83@gmail.com>
2026-04-06 22:01:13 +0530

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+export const transactions = [
+  { id: 1, date: "2026-04-01", category: "Salary", type: "income", amount: 5000 },
+  { id: 2, date: "2026-04-02", category: "Food", type: "expense", amount: 2000 },
+  { id: 3, date: "2026-04-03", category: "Shopping", type: "expense", amount: 3000 },
+];
